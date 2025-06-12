8972b3d372c92a07700a3ef06a2aa10e315e7c33
AbodiDawoud <mo7.daawoud@icloud.com>
2025-06-12 20:17:33 +0200

    Initial Commit

---
+++ Local-TTS/Local_TTSApp.swift
@@ -0,0 +1,15 @@
+//
+//  Local_TTSApp.swift
+//  Local-TTS
+    
+
+import SwiftUI
+
+@main
+struct Local_TTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
